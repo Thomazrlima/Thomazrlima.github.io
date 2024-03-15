9bc45b0d182c6a353be51b7c0829109d789a097e
thomazrlima <thomazlima211@gmail.com>
2024-03-15 12:57:22 -0300

    V12

--- static/script.js
+++ static/script.js
@@ -1,12 +1,9 @@
-
 document.addEventListener('DOMContentLoaded', () => {
     const sections = document.querySelectorAll("section");
     const navLinks = document.querySelectorAll('header nav a');
 
     window.onscroll = () => {
-
         sections.forEach(sec => {
-
             let top = window.scrollY;
             let offset = sec.offsetTop - 150;
             let height = sec.offsetHeight;
@@ -23,9 +20,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
-            console.log(entry);
             if (entry.isIntersecting) {
                 entry.target.classList.add('show');
+                if (entry.target.classList.contains('gameboy-text')) {
+                    typeText(0, entry.target);
+                }
             } else {
                 entry.target.classList.remove('show');
             }
@@ -33,5 +32,23 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     const hiddenElements = document.querySelectorAll(".hidden");
-    hiddenElements.forEach((el) => observer.observe(el));
+    hiddenElements.forEach((el) => {
+        observer.observe(el);
+    });
+
+    function typeText(index, target) {
+        const originalText = target.textContent;
+        target.textContent = '';
+
+        function typeChar(index) {
+            target.textContent = originalText.slice(0, index);
+            if (index < originalText.length) {
+                setTimeout(function () {
+                    typeChar(index + 1);
+                }, 100);
+            }
+        }
+
+        typeChar(0);
+    }
 });
